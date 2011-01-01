f6adab40a8e23f0b8576808622798921e478c0ab
Brian Alexander <balexand@gmail.com>
2010-12-31 18:37:05 -0800

    update readme and examples

--- examples/client_login_example.rb
+++ examples/client_login_example.rb
@@ -1,6 +1,8 @@
+# Highline is required for this example: gem install highline
+
 require 'rubygems'
 require 'gdata_plus'
-require 'highline/import' # gem install highline
+require 'highline/import'
 require 'typhoeus'
 
 # prompt for email/password
@@ -11,5 +13,6 @@ password = ask("Enter your password:  ") { |q| q.echo = "x" }
 authenticator = GDataPlus::Authenticator::ClientLogin.new(:service => "lh2", :source => "balexand-gdata_plus-1")
 authenticator.authenticate(email, password)
 
+# Make a signed request (fetch a list of Picasa albums in this case)
 response = authenticator.client.get("https://picasaweb.google.com/data/feed/api/user/default")
 puts response.inspect
--- examples/oauth_example.rb
+++ examples/oauth_example.rb
@@ -1,7 +1,6 @@
-# you don't need the following line if you've installed the gem
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-
+require 'rubygems'
 require 'gdata_plus'
+require 'nokogiri'
 require 'sinatra'
 require 'typhoeus'
 
@@ -9,29 +8,33 @@ require 'typhoeus'
 CONSUMER_KEY = ENV["GDATA_CONSUMER_KEY"]
 CONSUMER_SECRET = ENV["GDATA_CONSUMER_SECRET"]
 
-set :sessions, true # enable Sinatra sessions
+set :sessions, true
 
+# STEP #1: Obtain and store request token, and redirect browser to Google for authorization
 get "/" do
   oauth_callback = "#{request.scheme}://#{request.host}:#{request.port}/callback"
 
   authenticator = GDataPlus::Authenticator::OAuth.new(:consumer_key => CONSUMER_KEY, :consumer_secret => CONSUMER_SECRET)
   request_token = authenticator.fetch_request_token(:scope => "https://picasaweb.google.com/data/", :oauth_callback => oauth_callback)
 
-  # store authenticator in the session since we'll need it when exchanging request token for an access token
   session[:gdata_authenticator] = authenticator
 
   redirect request_token.authorize_url
 end
 
+# STEP #2: Exchange request token for access token when user is redirected back to your site
 get "/callback" do
   authenticator = session[:gdata_authenticator]
   authenticator.fetch_access_token(params[:oauth_verifier])
   redirect "/picasa_album_list"
 end
 
+# STEP #3: Make a signed request (fetch a list of Picasa albums in this case)
 get "/picasa_album_list" do
   authenticator = session[:gdata_authenticator]
   response = authenticator.client.get("https://picasaweb.google.com/data/feed/api/user/default")
 
-  response.inspect
+  feed = Nokogiri::XML(response.body)
+  album_titles = feed.xpath("//xmlns:entry/xmlns:title/text()").collect {|i| "#{i}"}
+  album_titles.collect {|title| "<li>#{title}</li>"}
 end
